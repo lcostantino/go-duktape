a3c4e08b370a903047c03fcf656d14a361fbcd1a
lcostantino <lcostantino@gmail.com>
2022-07-12 20:43:59 -0300

    Fix some warnings (only for x64, no plan for 32...)

--- duk_minimal_printf.c
+++ duk_minimal_printf.c
@@ -33,7 +33,7 @@ static size_t duk__format_long(char *str,
                                char pad,
                                int radix,
                                int neg_sign,
-                               unsigned long v) {
+                               unsigned long long v) {
 	char buf[24];  /* 2^64 = 18446744073709552000, length 20 */
 	char *required;
 	char *p;
@@ -92,7 +92,7 @@ static int duk__parse_pointer(const char *str, void **out) {
 	unsigned char ch;
 	int count;
 	int limit;
-	long val;  /* assume void * fits into long */
+	long long val;  /* assume void * fits into long */
 
 	/* We only need to parse what our minimal printf() produces, so that
 	 * we can check for a '0x' prefix, and assume all hex digits are
@@ -123,7 +123,7 @@ static int duk__parse_pointer(const char *str, void **out) {
 	 * JX pointer parsing also relies on that.
 	 */
 
-	*out = (void *) val;
+	*out = (long long *) val;
 	return 1;
 }
 
@@ -233,7 +233,7 @@ int duk_minimal_vsnprintf(char *str, size_t size, const char *format, va_list ap
 				v = va_arg(ap, void *);
 				DUK__WRITE_CHAR('0');
 				DUK__WRITE_CHAR('x');
-				off = duk__format_long(str, size, off, sizeof(void *) * 2, '0', 16, 0, (unsigned long) v);
+				off = duk__format_long(str, size, off, sizeof(void *) * 2, '0', 16, 0, (unsigned long long) v);
 				break;
 			} else {
 				/* Unrecognized, bail out early.  We could also emit the format
